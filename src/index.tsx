1c28ae3733bcfb12584e4d5a7f2d07f6891b6ecf
Kristian Selvaag <kristian_selvaag@trimble.com>
2018-12-05 11:07:47 +0100

    avoid merge conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,2 @@
 import './syncplots';
 import './mainplot';
-
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-
-const App = (props: {}) => <div>hello react</div>;
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('react-root')
-)
