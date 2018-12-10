7d50335051b4b19e0ffe5b0a4b01c0d93ba95759
Pieter Dhondt <pieter_dhondt@trimble.com>
2018-12-10 09:54:34 +0100

    updated nvdb

--- src/viewer.js
+++ src/viewer.js
@@ -136,12 +136,12 @@ customElements.whenDefined("webgl-viewer").then(async () => {
 
             if (!nvdbLoaded) {
                 document.getElementById('legend-div').style.visibility = "visible";
-                await viewer.load('./models/nvdb.trb', { transform: mat, resetCamera: false });
+                await viewer.load('./models/nvdb2.trb', { transform: mat, resetCamera: false });
                 // viewer.setCamera({ modelIds: ['./models/nvdb.trb'] });
                 nvdbLoaded = true;
             } else {
                 document.getElementById('legend-div').style.visibility = "hidden";
-                await viewer.unload('./models/nvdb.trb', { resetCamera: false });
+                await viewer.unload('./models/nvdb2.trb', { resetCamera: false });
                 nvdbLoaded = false;
             }
         });
